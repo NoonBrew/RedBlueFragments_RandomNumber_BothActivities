af759c4df07f56125e50683f77abd8e643693700
PearMyst <email>
2022-02-15 22:24:58 -0600

    Initial commit

---
+++ app/src/main/java/com/example/redbluefragments_randomnumber/BlueFragment.kt
@@ -0,0 +1,53 @@
+package com.example.redbluefragments_randomnumber
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BlueFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+const val RANDOM_NUMBER_VIEWED = "package com.example.redbluefragments_randomnumber.BlueFragment.RandViewed"
+class BlueFragment : Fragment() {
+
+    private lateinit var randomNumberTextView: TextView
+
+    private val randomNumberViewModel: RandomNumberViewModel by lazy {
+        ViewModelProvider(requireActivity()).get(RandomNumberViewModel::class.java)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_blue, container, false)
+        randomNumberTextView = view.findViewById(R.id.random_number_text_view)
+
+        showRandomNumber()
+        randomNumberTextView.setOnClickListener {
+            parentFragmentManager.setFragmentResult(RANDOM_NUMBER_VIEWED, Bundle.EMPTY)
+        }
+        return view
+    }
+
+    private fun showRandomNumber() {
+        randomNumberTextView.text = randomNumberViewModel.randomNumber.toString()
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         */
+
+        @JvmStatic
+        fun newInstance() = BlueFragment()
+    }
+}
---
+++ app/src/main/java/com/example/redbluefragments_randomnumber/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.redbluefragments_randomnumber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.setFragmentResultListener(RANDOM_NUMBER_GEN, this) {
+            requestKey, bundle -> // Swap out red fragment for blue fragment
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.fragmentContainerView, BlueFragment.newInstance())
+                    .addToBackStack("BLUE")
+                    .commit()
+        }
+
+        supportFragmentManager.setFragmentResultListener(RANDOM_NUMBER_VIEWED, this) {
+            requestKey, bundle -> // Swap out blue fragment for red fragment
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.fragmentContainerView, RedFragment.newInstance())
+                    .addToBackStack("RED")
+                    .commit()
+        }
+        // Options: 1. Adds, 2. Replaces.
+        // 1. Pause the red fragment, add the blue fragment "on top" <- Fine for lightweight fragments
+        // 2. Replace the red fragment with the blue fragment <- red fragment is destroyed.
+    }
+}
